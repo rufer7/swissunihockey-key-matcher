df579f07911ad8825d90931b64c10df31dbad339
Rufer Marc <rufermarc@gmail.com>
2015-02-20 22:24:59 +0100

    second test case added

--- src/test/java/web/rufer/swissunihockey/matcher/KeyMatcherTest.java
+++ src/test/java/web/rufer/swissunihockey/matcher/KeyMatcherTest.java
@@ -22,8 +22,11 @@ public class KeyMatcherTest {
     private Map<LocalDate, List<Integer>> keyMapTeam1 = new HashMap();
     private Map<LocalDate, List<Integer>> keyMapTeam2 = new HashMap();
     private LocalDate matchingDate = LocalDate.of(2015, 1, 1);
+    private LocalDate matchingDate2 = LocalDate.of(2015, 1, 2);
     private List<Integer> gameScheduleKeysTeam1 = Arrays.asList(0, 1, 2);
     private List<Integer> gameScheduleKeysTeam2 = Arrays.asList(1);
+    private List<Integer> secondGameScheduleKeysTeam1 = Arrays.asList(1);
+    private List<Integer> secondGameScheduleKeysTeam2 = Arrays.asList(0, 1);
 
     private KeyMatcher keyMatcher;
 
@@ -39,16 +42,38 @@ public class KeyMatcherTest {
         Team team1 = new Team(TEAM_NAME_1, nbrOfTeamsInLeague1, keyMapTeam1);
         Team team2 = new Team(TEAM_NAME_2, nbrOfTeamsInLeague2, keyMapTeam2);
         MatchingResult matchingResult = keyMatcher.findMatchesForTwoTeams(team1, team2);
-        assertEquals(createExpectedMatchingResult().getMatchesPerCombination(), matchingResult.getMatchesPerCombination());
+        assertEquals(createExpectedMatchingResultForTwoTeamsAndOneDate().getMatchesPerCombination(), matchingResult.getMatchesPerCombination());
     }
 
-    private MatchingResult createExpectedMatchingResult() {
+    private MatchingResult createExpectedMatchingResultForTwoTeamsAndOneDate() {
         MatchingResult matchingResult = new MatchingResult();
-
         matchingResult.setMatchForCombination("0 0", 0);
         matchingResult.setMatchForCombination("0 1", 1);
         matchingResult.setMatchForCombination("1 0", 0);
         matchingResult.setMatchForCombination("1 1", 1);
+        matchingResult.setMatchForCombination("2 0", 0);
+        matchingResult.setMatchForCombination("2 1", 1);
+        return matchingResult;
+    }
+
+    @Test
+    public void findMatchesForTwoTeamsAndTwoDatesFindsExpectedMatches() {
+        keyMapTeam1.put(matchingDate, gameScheduleKeysTeam1);
+        keyMapTeam1.put(matchingDate2, secondGameScheduleKeysTeam1);
+        keyMapTeam2.put(matchingDate, gameScheduleKeysTeam2);
+        keyMapTeam2.put(matchingDate2, secondGameScheduleKeysTeam2);
+        Team team1 = new Team(TEAM_NAME_1, nbrOfTeamsInLeague1, keyMapTeam1);
+        Team team2 = new Team(TEAM_NAME_2, nbrOfTeamsInLeague2, keyMapTeam2);
+        MatchingResult matchingResult = keyMatcher.findMatchesForTwoTeams(team1, team2);
+        assertEquals(createExpectedMatchingResultForTwoTeamsAndTwoDates().getMatchesPerCombination(), matchingResult.getMatchesPerCombination());
+    }
+
+    private MatchingResult createExpectedMatchingResultForTwoTeamsAndTwoDates() {
+        MatchingResult matchingResult = new MatchingResult();
+        matchingResult.setMatchForCombination("0 0", 0);
+        matchingResult.setMatchForCombination("0 1", 1);
+        matchingResult.setMatchForCombination("1 0", 1);
+        matchingResult.setMatchForCombination("1 1", 2);
         matchingResult.setMatchForCombination("2 0", 0);
         matchingResult.setMatchForCombination("2 1", 1);
         return matchingResult;

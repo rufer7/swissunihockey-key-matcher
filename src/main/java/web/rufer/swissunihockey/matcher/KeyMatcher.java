cbbdc448d525f63dfa1a4f0a3b7c95e0993b59b6
Rufer Marc <rufermarc@gmail.com>
2015-03-07 12:07:49 +0100

    javadoc added

--- src/main/java/web/rufer/swissunihockey/matcher/KeyMatcher.java
+++ src/main/java/web/rufer/swissunihockey/matcher/KeyMatcher.java
@@ -11,6 +11,13 @@ public class KeyMatcher {
 
     public static final String BLANKS = "       ";
 
+    /**
+     * Finds the matches for all combinations of two given leagues
+     *
+     * @param league1 league 1
+     * @param league2 league 2
+     * @return a matching result object, which contains the amount of matches per combination
+     */
     public MatchingResult findMatchesForTwoLeagues(League league1, League league2) {
         MatchingResult result = new MatchingResult();
         for (int i = 0; i < league1.getNbrOfTeamsInLeague(); i++) {
@@ -36,6 +43,14 @@ public class KeyMatcher {
         return matchesPerCombination;
     }
 
+    /**
+     * Checks, if the given date exits in both leagues
+     *
+     * @param date the given date
+     * @param keysPerDateForLeague1 keys per date for league 1
+     * @param keysPerDateForLeague2 keys per date for league 2
+     * @return true, if date exists in both leagues
+     */
     private boolean dateExistsInBothLeagues(LocalDate date, Map<LocalDate, List<Integer>> keysPerDateForLeague1, Map<LocalDate, List<Integer>> keysPerDateForLeague2) {
         return keysPerDateForLeague1.get(date) != null && keysPerDateForLeague2.get(date) != null;
     }

2049f9522c56f556288536fe217b7bf4589b38bb
rufer7 <marc.rufer@d-fens.net>
2018-03-26 12:50:12 +0200

    additional preparations for season 2018/2019

--- src/main/java/be/rufer/swissunihockey/HornetsKeyMatcherExecutor.java
+++ src/main/java/be/rufer/swissunihockey/HornetsKeyMatcherExecutor.java
@@ -30,8 +30,10 @@ public class HornetsKeyMatcherExecutor {
 
     private static final String U21C = "U21C";
     private static final String League1GF = "1. Liga GF";
+    private static final String League2GF = "2. Liga GF";
     private static final int NBR_OF_TEAMS_IN_U21C = 8;
     private static final int NBR_OF_TEAMS_IN_LEAGUE_1_GF = 12;
+    private static final int NBR_OF_TEAMS_IN_LEAGUE_2_GF = 10;
 
     public static void main(String[] args) {
         calculateAndPrintHornetsMatches();
@@ -58,14 +60,40 @@ public class HornetsKeyMatcherExecutor {
         u21C.put(LocalDate.of(2018, 11, 24), Arrays.asList(6, 4, 2, 7));
         u21C.put(LocalDate.of(2018, 12, 1), Arrays.asList(1, 3, 8, 5));
         u21C.put(LocalDate.of(2018, 12, 15), Arrays.asList(5, 6, 7, 8));
-        u21C.put(LocalDate.of(2018, 1, 12), Arrays.asList(3, 1, 2, 4));
-        u21C.put(LocalDate.of(2018, 1, 19), Arrays.asList(7, 5, 8, 6));
+        u21C.put(LocalDate.of(2019, 1, 12), Arrays.asList(3, 1, 2, 4));
+        u21C.put(LocalDate.of(2019, 1, 19), Arrays.asList(7, 5, 8, 6));
         u21C.put(LocalDate.of(2019, 1, 26), Arrays.asList(3, 2, 8, 7));
         u21C.put(LocalDate.of(2019, 2, 02), Arrays.asList(4, 6, 1, 2));
         u21C.put(LocalDate.of(2019, 2, 16), Arrays.asList(8, 1, 3, 5));
         u21C.put(LocalDate.of(2019, 3, 2), Arrays.asList(2, 4, 7, 6));
 
         return new League(U21C, NBR_OF_TEAMS_IN_U21C, u21C);
+    }
+
+    private static League createLeague2GF() {
+        // Herren 2. Liga GF
+        Map<LocalDate, List<Integer>> league2GF = new HashMap<>();
+        // Date, Keys that play at home at this date
+        league2GF.put(LocalDate.of(2018, 9, 15), Arrays.asList(1, 2, 3, 4, 5));
+        league2GF.put(LocalDate.of(2018, 9, 22), Arrays.asList(9, 8, 6, 7, 10));
+        league2GF.put(LocalDate.of(2018, 9, 29), Arrays.asList(3, 2, 4, 5, 10));
+        league2GF.put(LocalDate.of(2018, 10, 6), Arrays.asList(7, 1, 8, 6, 2));
+        league2GF.put(LocalDate.of(2018, 10, 27), Arrays.asList(9, 4, 10, 5, 3));
+        league2GF.put(LocalDate.of(2018, 11, 3), Arrays.asList(1, 6, 3, 2, 7));
+        league2GF.put(LocalDate.of(2018, 11, 10), Arrays.asList(10, 5, 4, 8, 9));
+        league2GF.put(LocalDate.of(2018, 11, 17), Arrays.asList(4, 3, 1, 6, 2));
+        league2GF.put(LocalDate.of(2018, 11, 24), Arrays.asList(8, 7, 10, 5, 9));
+        league2GF.put(LocalDate.of(2018, 12, 1), Arrays.asList(6, 7, 8, 9, 10));
+        league2GF.put(LocalDate.of(2018, 12, 8), Arrays.asList(5, 4, 2, 3, 1));
+        league2GF.put(LocalDate.of(2018, 12, 15), Arrays.asList(6, 1, 7, 8, 9));
+        league2GF.put(LocalDate.of(2019, 1, 5), Arrays.asList(5, 9, 10, 4, 3));
+        league2GF.put(LocalDate.of(2019, 1, 12), Arrays.asList(8, 2, 7, 6, 1));
+        league2GF.put(LocalDate.of(2019, 1, 19), Arrays.asList(8, 10, 4, 5, 9));
+        league2GF.put(LocalDate.of(2019, 2, 9), Arrays.asList(2, 3, 1, 7, 6));
+        league2GF.put(LocalDate.of(2019, 2, 16), Arrays.asList(5, 10, 7, 8, 9));
+        league2GF.put(LocalDate.of(2019, 3, 2), Arrays.asList(2, 6, 4, 1, 3));
+
+        return new League(League2GF, NBR_OF_TEAMS_IN_LEAGUE_2_GF, league2GF);
     }
 
     private static League createLeague1GF() {

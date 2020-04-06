504e6c2574f541dc280513765db9745ea545ee7c
rufer7 <marc.rufer@d-fens.net>
2020-04-06 22:37:29 +0200

    Key representation fixed (starting at 1 instead o 0)

--- src/main/java/be/rufer/swissunihockey/matcher/KeyMatcher.java
+++ src/main/java/be/rufer/swissunihockey/matcher/KeyMatcher.java
@@ -33,8 +33,8 @@ public class KeyMatcher {
      */
     public MatchingResult findMatchesForTwoLeagues(League league1, League league2) {
         MatchingResult result = new MatchingResult();
-        for (int i = 0; i < league1.getNbrOfTeamsInLeague(); i++) {
-            for (int j = 0; j < league2.getNbrOfTeamsInLeague(); j++) {
+        for (int i = 1; i <= league1.getNbrOfTeamsInLeague(); i++) {
+            for (int j = 1; j <= league2.getNbrOfTeamsInLeague(); j++) {
                 result.setMatchForCombination(i + Constants.TAB + j, calculateMatchesPerCombination(league1, league2, i, j));
             }
         }
--- src/test/java/be/rufer/swissunihockey/matcher/KeyMatcherTest.java
+++ src/test/java/be/rufer/swissunihockey/matcher/KeyMatcherTest.java
@@ -30,17 +30,17 @@ public class KeyMatcherTest {
 
     private static final String LEAGUE_NAME_1 = "league1";
     private static final String LEAGUE_NAME_2 = "league2";
-    private static final int nbrOfTeamsInLeague1 = 3;
+    private static final int nbrOfTeamsInLeague1 = 4;
     private static final int nbrOfTeamsInLeague2 = 2;
     private Map<LocalDate, List<Integer>> keysPerDateMapLeague1 = new HashMap<>();
     private Map<LocalDate, List<Integer>> keysPerDateMapLeague2 = new HashMap<>();
-    private LocalDate matchingDate = LocalDate.of(2015, 1, 1);
-    private LocalDate matchingDate2 = LocalDate.of(2015, 1, 2);
-    private LocalDate matchingDate3 = LocalDate.of(2015, 1, 3);
-    private List<Integer> gameScheduleKeysLeague1 = Arrays.asList(0, 1, 2);
+    private LocalDate matchingDate1 = LocalDate.of(2015, 1, 1);
+    private LocalDate matchingDate2 = LocalDate.of(2015, 1, 8);
+    private LocalDate matchingDate3 = LocalDate.of(2015, 1, 15);
+    private List<Integer> gameScheduleKeysLeague1 = Arrays.asList(1, 2);
     private List<Integer> gameScheduleKeysLeague2 = Collections.singletonList(1);
-    private List<Integer> secondGameScheduleKeysLeague1 = Collections.singletonList(1);
-    private List<Integer> secondGameScheduleKeysLeague2 = Arrays.asList(0, 1);
+    private List<Integer> secondGameScheduleKeysLeague1 = Arrays.asList(2, 3);
+    private List<Integer> secondGameScheduleKeysLeague2 = Collections.singletonList(1);
 
     private KeyMatcher keyMatcher;
 
@@ -51,8 +51,8 @@ public class KeyMatcherTest {
 
     @Test
     public void findMatchesForTwoLeaguesAndOneEqualDateFindsExpectedMatches() {
-        keysPerDateMapLeague1.put(matchingDate, gameScheduleKeysLeague1);
-        keysPerDateMapLeague2.put(matchingDate, gameScheduleKeysLeague2);
+        keysPerDateMapLeague1.put(matchingDate1, gameScheduleKeysLeague1);
+        keysPerDateMapLeague2.put(matchingDate1, gameScheduleKeysLeague2);
         League league1 = new League(LEAGUE_NAME_1, nbrOfTeamsInLeague1, keysPerDateMapLeague1);
         League league2 = new League(LEAGUE_NAME_2, nbrOfTeamsInLeague2, keysPerDateMapLeague2);
         MatchingResult matchingResult = keyMatcher.findMatchesForTwoLeagues(league1, league2);
@@ -61,20 +61,22 @@ public class KeyMatcherTest {
 
     private MatchingResult createExpectedMatchingResultForTwoLeaguesAndOneEqualDate() {
         MatchingResult matchingResult = new MatchingResult();
-        matchingResult.setMatchForCombination("0" + Constants.TAB + "0", 0);
-        matchingResult.setMatchForCombination("0" + Constants.TAB + "1", 1);
-        matchingResult.setMatchForCombination("1" + Constants.TAB + "0", 0);
         matchingResult.setMatchForCombination("1" + Constants.TAB + "1", 1);
-        matchingResult.setMatchForCombination("2" + Constants.TAB + "0", 0);
+        matchingResult.setMatchForCombination("1" + Constants.TAB + "2", 0);
         matchingResult.setMatchForCombination("2" + Constants.TAB + "1", 1);
+        matchingResult.setMatchForCombination("2" + Constants.TAB + "2", 0);
+        matchingResult.setMatchForCombination("3" + Constants.TAB + "1", 0);
+        matchingResult.setMatchForCombination("3" + Constants.TAB + "2", 0);
+        matchingResult.setMatchForCombination("4" + Constants.TAB + "1", 0);
+        matchingResult.setMatchForCombination("4" + Constants.TAB + "2", 0);
         return matchingResult;
     }
 
     @Test
     public void findMatchesForTwoLeaguesAndTwoDatesEqualFindsExpectedMatches() {
-        keysPerDateMapLeague1.put(matchingDate, gameScheduleKeysLeague1);
+        keysPerDateMapLeague1.put(matchingDate1, gameScheduleKeysLeague1);
         keysPerDateMapLeague1.put(matchingDate2, secondGameScheduleKeysLeague1);
-        keysPerDateMapLeague2.put(matchingDate, gameScheduleKeysLeague2);
+        keysPerDateMapLeague2.put(matchingDate1, gameScheduleKeysLeague2);
         keysPerDateMapLeague2.put(matchingDate2, secondGameScheduleKeysLeague2);
         League league1 = new League(LEAGUE_NAME_1, nbrOfTeamsInLeague1, keysPerDateMapLeague1);
         League league2 = new League(LEAGUE_NAME_2, nbrOfTeamsInLeague2, keysPerDateMapLeague2);
@@ -84,20 +86,22 @@ public class KeyMatcherTest {
 
     private MatchingResult createExpectedMatchingResultForTwoLeaguesAndTwoEqualDates() {
         MatchingResult matchingResult = new MatchingResult();
-        matchingResult.setMatchForCombination("0" + Constants.TAB + "0", 0);
-        matchingResult.setMatchForCombination("0" + Constants.TAB + "1", 1);
-        matchingResult.setMatchForCombination("1" + Constants.TAB + "0", 1);
-        matchingResult.setMatchForCombination("1" + Constants.TAB + "1", 2);
-        matchingResult.setMatchForCombination("2" + Constants.TAB + "0", 0);
-        matchingResult.setMatchForCombination("2" + Constants.TAB + "1", 1);
+        matchingResult.setMatchForCombination("1" + Constants.TAB + "1", 1);
+        matchingResult.setMatchForCombination("1" + Constants.TAB + "2", 0);
+        matchingResult.setMatchForCombination("2" + Constants.TAB + "1", 2);
+        matchingResult.setMatchForCombination("2" + Constants.TAB + "2", 0);
+        matchingResult.setMatchForCombination("3" + Constants.TAB + "1", 1);
+        matchingResult.setMatchForCombination("3" + Constants.TAB + "2", 0);
+        matchingResult.setMatchForCombination("4" + Constants.TAB + "1", 0);
+        matchingResult.setMatchForCombination("4" + Constants.TAB + "2", 0);
         return matchingResult;
     }
 
     @Test
     public void findMatchesForTwoLeaguesAndDifferentDatesFindsExpectedMatches() {
-        keysPerDateMapLeague1.put(matchingDate, gameScheduleKeysLeague1);
+        keysPerDateMapLeague1.put(matchingDate1, gameScheduleKeysLeague1);
         keysPerDateMapLeague1.put(matchingDate2, secondGameScheduleKeysLeague1);
-        keysPerDateMapLeague2.put(matchingDate, gameScheduleKeysLeague2);
+        keysPerDateMapLeague2.put(matchingDate1, gameScheduleKeysLeague2);
         keysPerDateMapLeague2.put(matchingDate2, secondGameScheduleKeysLeague2);
         keysPerDateMapLeague2.put(matchingDate3, secondGameScheduleKeysLeague2);
         League league1 = new League(LEAGUE_NAME_1, nbrOfTeamsInLeague1, keysPerDateMapLeague1);
